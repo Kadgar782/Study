e4bf65cb67e045bab7cae4268f76cb5d702cd655
Kadgar782 <93835158+Kadgar782@users.noreply.github.com>
2022-08-02 22:55:09 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,49 +1,68 @@
+//creating HTML elements
+
 const createdElements = [["h1"], ["p"], ["span"]];
 
 function createHTML (typeOfCreatedElement, locationForCreatedElement) {
 
-const createdElement = document.createElement(typeOfCreatedElement);
+  const createdElement = document.createElement(typeOfCreatedElement);
 
-locationForCreatedElement.appendChild( createdElement );
-createdElement.innerHTML =  typeOfCreatedElement;
+  locationForCreatedElement.appendChild( createdElement );
+  createdElement.innerHTML =  typeOfCreatedElement;
 
-return createdElement;
+  return createdElement;
 }
+
 const divInner = document.getElementsByClassName("inner")[0];
 const createdH1 = createHTML( createdElements[0], divInner);
-const createdParagraph = createHTML( createdElements[1],createdH1 );
-const createdSpan = createHTML( createdElements[2], createdParagraph  );
+const createdParagraph = createHTML( createdElements[1],createdH1);
+const createdSpan = createHTML( createdElements[2], createdParagraph);
 
 //Json
 
-const requestURL  = "https://jsonplaceholder.typicode.com/posts";
-const request = new XMLHttpRequest();
-request.open("GET", requestURL);
-request.responseType = "json";
-request.send();
 
-request.onload = function() {
-   const usersInfo = request.response;
-   console.log(usersInfo);
-  
- }
+ fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    const usersInfo = data;
+    console.log(usersInfo)
+  });
 
 
-// function elementFromHTML(html) {
-//    const template = document.createElement("template");
+//alternative Json
 
-//    template.innerHTML = html.trim();
+ // const requestURL  = "https://jsonplaceholder.typicode.com/posts";
+ // const request = new XMLHttpRequest();
+ // request.open("GET", requestURL);
+ // request.responseType = "json";
+ // request.send();
 
-//    return template.content.firstElementChild;
+ // request.onload = function() {
+ //    const usersInfo = request.response;
+ //    console.log(usersInfo);
+ //  }
 
-// }
 
-//   const thread = elementFromHTML (`
-//   <div class="container">
-//   <h1>Заголовок</h1>
+//Alternative createdElement
+
+
+ // function elementFromHTML(html) {
+ //    const template = document.createElement("template"); 
+
+ //    template.innerHTML = html.trim();
+
+ //    return template.content.firstElementChild;
+
+ // }
+
+ //   const thread = elementFromHTML (`
+ //   <div class="container">
+ //   <h1>Заголовок</h1>
  
-//   <p>а</p>
-//   <p class="userID">4564</p>
-//   </div>
-//   `)
+ //   <p>а</p>
+ //   <p class="userID">4564</p>
+ //   </div>
+ //   `)
+
 
