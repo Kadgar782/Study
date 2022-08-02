eba758617f905252ede51ed582ca7d814d13b4a6
Kadgar782 <93835158+Kadgar782@users.noreply.github.com>
2022-08-02 20:35:09 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const createdElements = [["h1"], ["p"], ["span"]];
+
+function createHTML (typeOfCreatedElement, locationForCreatedElement) {
+
+const createdElement = document.createElement(typeOfCreatedElement);
+
+locationForCreatedElement.appendChild( createdElement );
+createdElement.innerHTML =  typeOfCreatedElement;
+
+return createdElement;
+}
+const divInner = document.getElementsByClassName("inner")[0];
+const createdH1 = createHTML( createdElements[0], divInner);
+const createdParagraph = createHTML( createdElements[1],createdH1 );
+const createdSpan = createHTML( createdElements[2], createdParagraph  );
+
+
+
+// function elementFromHTML(html) {
+//    const template = document.createElement("template");
+
+//    template.innerHTML = html.trim();
+
+//    return template.content.firstElementChild;
+
+// }
+
+//   const thread = elementFromHTML (`
+//   <div class="container">
+//   <h1>Заголовок</h1>
+ 
+//   <p>а</p>
+//   <p class="userID">4564</p>
+//   </div>
+//   `)
+
