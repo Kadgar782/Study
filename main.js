b3dbf9a64207c5f5efa72558f9b890d89a4a899d
Kadgar782 <93835158+Kadgar782@users.noreply.github.com>
2022-08-23 20:28:17 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,42 +1,33 @@
-//Getting Post content
-fetch("https://jsonplaceholder.typicode.com/posts")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    const posts = data;
-    console.log(posts);
-    // Getting User names
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const users = data;
-        console.log(users);
-        // Changing ID
-        const mappedPosts = posts.map((p) => {
-          const createdBy = users.find((u) => u.id === p.userId);
+async function loadPage() {
+ //Getting Post content
+  let responsePosts = await fetch("https://jsonplaceholder.typicode.com/posts");
+  let posts = await responsePosts.json();
+ // Getting User names
+  let responseUsers = await fetch("https://jsonplaceholder.typicode.com/users");
+  let users = await responseUsers.json();
+ // Changing ID
+  const mappedPosts = posts.map((p) => {
+    const createdBy = users.find((u) => u.id === p.userId);
 
-          return {
-            ...p,
-            userId: createdBy ? createdBy.username : "Unknown user",
-          };
-        });
-
-        console.log(mappedPosts);
-
-        //Creating Post
-        for (let post of mappedPosts) {
-          const container = document.getElementsByClassName("outer")[0];
-          const resultedPost = createPost(post);
-          const postWrapper = document.createElement("div");
-          postWrapper.classList.add("inner");
-          postWrapper.innerHTML = resultedPost;
-          container.appendChild(postWrapper);
-        }
-      });
+    return {
+      ...p,
+      userId: createdBy ? createdBy.username : "Unknown user",
+    };
   });
+
+  console.log(mappedPosts);
+  //Creating Post
+  for (let post of mappedPosts) {
+    const container = document.getElementsByClassName("outer")[0];
+    const resultedPost = createPost(post);
+    const postWrapper = document.createElement("div");
+    postWrapper.classList.add("inner");
+    postWrapper.innerHTML = resultedPost;
+    container.appendChild(postWrapper);
+  }
+}
+loadPage();
+
 //creating HTML elements
 
 function createPost(post) {
