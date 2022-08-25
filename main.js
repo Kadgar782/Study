cc2e56430dd61e5581a7a53c8999ea06f7c2a2e6
Kadgar782 <93835158+Kadgar782@users.noreply.github.com>
2022-08-25 20:31:25 +0600

    Update main.js

--- main.js
+++ main.js
@@ -2,34 +2,24 @@ async function loadPage() {
 //Getting Post content
   let responsePosts = await fetch("https://jsonplaceholder.typicode.com/posts");
   let posts = await responsePosts.json();
-//Getting User names
+// Getting User names
   let responseUsers = await fetch("https://jsonplaceholder.typicode.com/users");
   let users = await responseUsers.json();
-//Gettinf Photos
+// Gettinf Photos
  let responsePhotos = await fetch ("https://jsonplaceholder.typicode.com/photos");
  let photos = await responsePhotos.json();
 //Add Avatar
-const postsWithAvatars = posts.map((p) => {
-  const createdBy = photos.find((u) => u.id === p.userId);
+const mappedPosts = posts.map((p) => {
+  const avatars = photos.find((u) => u.id === p.userId); // userId в постах 
+// Changing ID
+  const createdBy = users.find((u) => u.id === p.userId);
   return {
     ...p, 
-    createdBy
+    avatars,
+    userId: createdBy ? createdBy.username : "Unknown user",
   }
 });
-console.log(postsWithAvatars)
 
-  
-// Changing ID
-  const mappedPosts = postsWithAvatars.map((p) => {
-    const createdBy = users.find((u) => u.id === p.userId);
-
-    return {
-      ...p,
-      userId: createdBy ? createdBy.username : "Unknown user",
-    };
-  });
-
-  console.log(mappedPosts);
   //Creating Post
   for (let post of mappedPosts) {
     const container = document.getElementsByClassName("outer")[0];
@@ -47,6 +37,6 @@ loadPage();
 function createPost(post) {
   return `
     <h1>${post.title}</h1>
-    <p>${post.body}<span>${post.userId}<img class="Avatar" src=${post.createdBy.thumbnailUrl}></span></p>
+    <p>${post.body}<span>${post.userId}<img class="Avatar" src=${post.avatars.thumbnailUrl}></span></p>
   `;
 }
